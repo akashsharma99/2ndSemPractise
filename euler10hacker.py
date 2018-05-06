41531af928e14a841f1c6575b864350f86b34083
akashsharma99 <akash.asharma.sharma11@gmail.com>
2018-05-06 23:27:12 +0530

    all test cases passed. previously last 2 cases showed TLE. Here i made 2 as prime by default so i didnt have to traverse through from all multiples of 2

--- euler10hacker.py
+++ euler10hacker.py
@@ -1,23 +1,19 @@
-prime=[]
 
-for i in range(1000001):
-    prime.append(1)
-p=2
-while(p*p<=1000000):
-    if (prime[p] == 1):
+prime=[1] * 1000001
+sum=[0] * 1000001
+prime[0],prime[1]=0,0
+
+for p,pri in enumerate(prime):
+    if (pri):
+        sum[p]=sum[p-1]+p
         i=p*p
         while(i<=1000000):
             prime[i]=0
             i+=p
-    p+=1
-def SumOfPrimes(n):
-    s=0
-    for i in range(2,n):
-        if(prime[i]==1):
-            s=s+i;
-    print(s)
+    else:
+        sum[p]=sum[p-1]
 
 t = int(input())
 for a0 in range(t):
     n = int(input())
-    SumOfPrimes(n+1)
+    print(sum[n])
