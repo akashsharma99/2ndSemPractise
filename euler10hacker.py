1a52985178ddeb8aed0afbdf77c297cc805f4878
akashsharma99 <akash.asharma.sharma11@gmail.com>
2018-05-06 23:32:04 +0530

    explained why prime[1]=0

--- euler10hacker.py
+++ euler10hacker.py
@@ -2,7 +2,7 @@
 prime=[1] * 1000001
 sum=[0] * 1000001
 prime[0],prime[1]=0,0
-
+#ensures that multiples of one are not traversed unlike previously where i had prime[1] as true
 for p,pri in enumerate(prime):
     if (pri):
         sum[p]=sum[p-1]+p
